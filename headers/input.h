dd864d76a64ef322e307ad9345c352db2845fe8e
Lucifer9845 <mohit.baroliya23@gmail.com>
2022-08-13 12:06:39 +0530

    new files added

---
+++ headers/input.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int input() {
+
+    FILE *file;
+    char c;
+    char *s = NULL;
+    int i = 0;
+    file=fopen("test.txt","rt");
+
+    while((c=fgetc(file))!=EOF){
+        printf("%c",c);
+        s[i++] = c;
+    }
+
+    fclose(file);
+    printf("%s", s);
+    return 0;
+}
---
+++ main/main.cpp
@@ -0,0 +1,7 @@
+#include "D:\college\4th sem\DAA\code\Huffman_code\Huffman_code\headers\input.h"
+
+int main()
+{
+    input();
+    return 0;
+}
